2fa606da06be2b8fa5384ef3dd7164c69db5d414
Flayre <mariesamantha.f@gmail.com>
2023-10-21 05:40:05 -0400

    Added back camera feature

--- frontend/Pages/ScannerScreen.tsx
+++ frontend/Pages/ScannerScreen.tsx
@@ -1,12 +1,42 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { StyleSheet, Text, SafeAreaView, Image } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
 
-export default function ScannerScreen({ navigation }: any) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Scanner Screen</Text>
-        </View>
-    );
+export default function App() {
+  let cameraRef = useRef();
+  const [hasCameraPermission, setHasCameraPermission] = useState(false);
+  const [photo, setPhoto] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+      setHasCameraPermission(cameraPermission.status === "granted");
+    })();
+  }, []);
+
+  if (hasCameraPermission === undefined) {
+    return <Text>Requesting permissions...</Text>;
+  } else if (!hasCameraPermission) {
+    return <Text>Permission for camera not granted. Please change this in settings.</Text>;
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Camera style={styles.camera} ref={cameraRef} type={Camera.Constants.Type.back} />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  camera: {
+    flex: 1,
+    width: '100%',
+  },
+});
