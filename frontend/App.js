ddc7c43eed1e1baa637b3b7d236d346de2784e05
Flayre <mariesamantha.f@gmail.com>
2023-10-21 03:52:19 -0400

    Revert "Initial experiment w maps"
    This reverts commit d07086bf31dc69feed20aa3127c6137856c4877d.

--- frontend/App.js
+++ frontend/App.js
@@ -1,19 +1,20 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text>Recycling Centers Near Me</Text>
-			<Map />
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
