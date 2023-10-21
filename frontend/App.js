ad5aac04209942eb60a584ee59605569f192ee28
William Gervasio <wilpgerv@telus.net>
2023-10-21 01:27:40 -0700

    merge into branch

---
+++ frontend/App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { View, Text } from 'react-native';
+import Navbar from './components/Navbar/Navbar';
+
+export default function App() {
+  return (
+    <Navbar/>
+  );
+}
